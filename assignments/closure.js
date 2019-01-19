58595ea31217d0acbd99f3459d8893dc0892ab14
kaiserawu <kaiser.a.wu@gmail.com>
2019-01-19 10:12:29 -0800

    Complete closure challenge 2

--- assignments/closure.js
+++ assignments/closure.js
@@ -12,7 +12,16 @@ displayColor();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  };
 };
+
+let newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
