f12b90631f8689a62fa66a28192fef4d39b0d6a5
kaiserawu <kaiser.a.wu@gmail.com>
2019-01-19 10:29:00 -0800

    Complete closure stretch challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -33,4 +33,21 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+
+  return {
+    increment: () => ++count,
+    decrement: () => --count
+  }
 };
+
+let testCounter = counterFactory();
+console.log(testCounter);
+console.log(testCounter.increment());
+console.log(testCounter.increment());
+console.log(testCounter.increment());
+console.log(testCounter.increment());
+console.log(testCounter.decrement());
+console.log(testCounter.increment());
+console.log(testCounter.decrement());
+console.log(testCounter.decrement());
